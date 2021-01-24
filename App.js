0d0051bab94c5cb40068fc565c5258ace10fd4f5
Bilal Mir <bilalmir135@gmail.com>
2021-01-25 00:44:07 +0500

    Add ImagePicker

--- App.js
+++ App.js
@@ -1,36 +1,21 @@
 import React, { useState } from 'react';
-import { View, StyleSheet } from 'react-native';
-
-import WelcomeScreen from './app/screens/WelcomeScreen';
-import ViewImageScreen from './app/screens/ViewImageScreen';
-import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
-import MessagesScreen from './app/screens/MessagesScreen';
-import AccountScreen from './app/screens/AccountScreen';
-import ListingScreen from './app/screens/ListingScreen';
-import Card from './app/components/Card';
-
 import Screen from './app/components/Screen';
-import Icon from './app/components/Icon';
-import ListItem from './app/components/ListItem';
-import AppTextInput from './app/components/AppTextInput';
-import AppPicker from './app/components/AppPicker';
-import LoginScreen from './app/screens/LoginScreen';
-import ListEditingScreen from './app/screens/ListEditingScreen';
+import ImageInputList from './app/components/ImageInputList';
 
 export default function App() {
-  return <ListEditingScreen />;
-}
+  const [imageUris, setImageUris] = useState([]);
 
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#f8f4f4',
-    padding: 20,
-    paddingTop: 100,
-  },
-});
+  const handleAdd = uri => {
+    setImageUris([...imageUris, uri]);
+  };
 
-{
-  /* <View style={styles.container}>
-      <Card title='Red jacket for sale' subTitle='$100' image={require('./app/assets/jacket.jpg')} />
-    </View> */
+  const handleRemove = uri => {
+    setImageUris(imageUris.filter(imageUri => imageUri !== uri));
+  };
+
+  return (
+    <Screen>
+      <ImageInputList imageUris={imageUris} onAddImage={handleAdd} onRemoveImage={handleRemove} />
+    </Screen>
+  );
 }
